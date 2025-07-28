d1073c7d388876f349a63e87292b6fdaedc7c0bd
schochastics <david@schochastics.net>
2025-07-28 12:05:28 +0200

    added sentence

--- R/dex.R
+++ R/dex.R
@@ -6,6 +6,14 @@ sample_digits <- function(n, m = 4) {
   sprintf(paste0("%0", m, "d"), sample(0:max_val, n, replace = TRUE))
 }
 
+sample_words <- function(words, n) {
+  words[sample.int(
+    length(words),
+    size = n,
+    replace = TRUE
+  )]
+}
+
 #' Generate Random IDs Using Adjectives and Pokemon Names
 #'
 #' @description
@@ -55,18 +63,9 @@ dexid <- function(
   if (!all(gen %in% 1:9)) {
     stop("Generations must be between 1 and 9.")
   }
-  adj_sample <- adjectives[sample.int(
-    length(adjectives),
-    size = n,
-    replace = TRUE
-  )]
-
+  adj_sample <- sample_words(adjectives, n)
   pokemon_gen <- pokemon[generation %in% gen]
-  poke_sample <- pokemon[sample.int(
-    length(pokemon_gen),
-    size = n,
-    replace = TRUE
-  )]
+  poke_sample <- sample_words(pokemon_gen, n)
 
   if (add_digits) {
     poke_sample <- paste0(poke_sample, sample_digits(n))
@@ -74,4 +73,62 @@ dexid <- function(
 
   dex <- paste0(adj_sample, " ", poke_sample)
   snakecase::to_any_case(dex, case = case, ...)
+}
+
+#' Generate Random sentences of Pokémon using moves
+#'
+#' @inheritParams dexid
+#' @return A character vector of length `n` containing the generated sentences.
+#'
+#' @examples
+#' dex_sentence(3)
+#' dex_sentence(5)
+#'
+#' @export
+dex_sentence <- function(
+  n,
+  gen = 1:9,
+  case = c(
+    "sentence",
+    "snake",
+    "small_camel",
+    "big_camel",
+    "screaming_snake",
+    "parsed",
+    "mixed",
+    "lower_upper",
+    "upper_lower",
+    "swap",
+    "all_caps",
+    "lower_camel",
+    "upper_camel",
+    "internal_parsing",
+    "none",
+    "flip",
+    "random",
+    "title"
+  ),
+  ...
+) {
+  case <- match.arg(case)
+  if (!all(gen %in% 1:9)) {
+    stop("Generations must be between 1 and 9.")
+  }
+  adj_sample <- sample_words(adjectives, n)
+  pokemon_gen <- pokemon[generation %in% gen]
+  poke_sample <- sample_words(pokemon_gen, n)
+  adverb_sample <- sample_words(adverbs, n)
+  move_sample <- sample_words(moves, n)
+  sentence <- paste0(
+    adj_sample,
+    " ",
+    poke_sample,
+    " ",
+    " uses ",
+    move_sample,
+    " ",
+    adverb_sample,
+    "."
+  )
+  snakecase::to_any_case(sentence, case = case, ...)
 }
--- data-raw/data.R
+++ data-raw/data.R
@@ -1,11 +1,24 @@
 adjectives <- readLines(
   "https://gist.githubusercontent.com/hugsy/8910dc78d208e40de42deb29e62df913/raw/eec99c5597a73f6a9240cab26965a8609fa0f6ea/english-adjectives.txt"
 )
+
+adverbs <- readLines(
+  "https://raw.githubusercontent.com/janester/mad_libs/refs/heads/master/List%20of%20Adverbs.txt"
+)
+
 pokemon <- readLines(
   "https://gist.githubusercontent.com/killshot13/5b45c0089c3b1a19028bec38aad8fa46/raw/10d30ab4a74ac7e083a3cc6135c605379ddee952/pokemon.txt"
 )
 
 pokemon <- stringr::str_replace_all(pokemon, "\\.", "")
+
+lines <- readLines(
+  "https://raw.githubusercontent.com/pcattori/pokemon/refs/heads/master/pokemon/data/moves.json"
+)
+wrapped <- paste0("[", paste(lines, collapse = ","), "]")
+moves_json <- jsonlite::fromJSON(wrapped)
+moves <- moves_json$name
+
 
 library(rvest)
 url <- "https://bulbapedia.bulbagarden.net/wiki/Generation"
@@ -18,7 +31,9 @@ generation <- rep(1:9, generations)
 
 usethis::use_data(
   adjectives,
+  adverbs,
   pokemon,
+  moves,
   generation,
   internal = TRUE,
   overwrite = TRUE
